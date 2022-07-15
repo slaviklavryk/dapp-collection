0f7b2b50459b2a4c99b4732bbea50d132ab28e1c
lavryk@gmail.com <lavryk@gmail.com>
2022-07-15 15:01:05 +0300

    Added header and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import {ethers} from "ethers";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { GiBirdClaw } from "react-icons/gi";
 
 function App() {
-  const [account, setAccount] = useState(null);
+  const [account, setAccount] = useState("");
 
   const initConnection = async () => {
     if (typeof window.ethereum !== "undefined"){
@@ -14,12 +15,28 @@ function App() {
     } else {
       console.log("Please install Metamask");
     }
-  }
+  };
+
+  useEffect(() => {
+    initConnection();
+  }, []);
 
   return (
-    <div>
-      <button onClick={initConnection}>Connect</button>
-      <p>{account}</p>
+    <div className="page">
+      <div className="header">
+        <img src={require('./assets/images/logo.jpg')} className="artIcon"/>
+        <p>
+          11/15
+          <span>
+            <GiBirdClaw style={{marginLeft: "5px"}}/>
+          </span>
+        </p>
+        {account == "" ? (
+          <button onClick={initConnection} className="button">Connect</button>
+        ) : (
+          <p>...{account.substring(account.length - 7)}</p>
+        )}  
+      </div>
     </div>
     
   );
